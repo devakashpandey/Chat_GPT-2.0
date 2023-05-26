d6518af43b84a47e7a0416a0db84e3462eaba484
DevAkky <developerakky@gmail.com>
2023-05-26 09:19:00 +0530

    ui done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import ChatBody from "./components/chatBody/ChatBody";
-import userInput from "./components/userInput/userInput";
+import UserInput from "./components/userInput/UserInput";
 
 function App() {
   return (
     <>
       <div
-        className="bg-[#1A232E] h-screen py-6 relative sm:px-28 text-white 
+        className="bg-[#1A232E] h-screen py-6 relative sm:px-28 px-10 text-white 
     overflow-hidden flex flex-col justify-between align-middle"
       >
         {/* gradient */}
@@ -26,10 +26,8 @@ function App() {
         </div>
 
         {/* user input */}
-        <div className="full max-w-4xl min-w-[20rem] self-center">
-          {" "}
-          <userInput />
-        </div>
+        <div className="full max-w-4xl min-w-[20rem] self-center"></div>
+        <UserInput />
       </div>
     </>
   );
--- src/components/userInput/userInput.js
+++ src/components/userInput/userInput.js
@@ -1,14 +1,29 @@
 import React from "react";
+import sendIcon from "../../assets/send.png";
 
-const userInput = () => {
+const UserInput = () => {
   return (
     <>
-      <div>
-        <textarea />
-        <img src="" alt="" />
+      <div
+        className="w-full bg-white bg-opacity-10 max-h-40 py-4
+      rounded-lg overflow-hidden relative"
+      >
+        {/* <textarea
+          rows={1}
+          className="border-0 bg-transparent outline-none w-[90%] px-5"
+        /> */}
+        <input className="border-0 bg-transparent outline-none w-[90%] px-5" />
+
+        <img
+          src={sendIcon}
+          width={20}
+          alt="sendIcon"
+          className="absolute top-4 right-3 hover:cursor-pointer ease-in duration-100
+          hover:scale-125"
+        />
       </div>
     </>
   );
 };
 
-export default userInput;
+export default UserInput;
